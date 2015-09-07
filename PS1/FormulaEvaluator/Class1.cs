3191e46f28b30ad0079fae3e90f27c32b8064ce1
Ryan Fletcher <buck417@gmail.com>
2015-09-07 14:10:52 -0600

    New Initial with C#

---
+++ PS1/FormulaEvaluator/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator{
+    
+
+    Public Static Class Evaluator{
+
+    public delegate int Lookup(String v);
+    String[] substrings = Regex.Split(s, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+        
+    
+
+      public static int Evaluate(String exp, Lookup variableEvaluator)
+      {
+        int x;
+      }
+}
+
+}
