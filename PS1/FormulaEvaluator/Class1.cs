5504f4de7ee1e40d96f90739fccd810be8ab3d61
Ryan Fletcher <buck417@gmail.com>
2015-09-08 11:36:01 -0600

    Added method variables to Evaluate method

--- PS1/FormulaEvaluator/Class1.cs
+++ PS1/FormulaEvaluator/Class1.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FormulaEvaluator{
     
 
-    Public Static Class Evaluator{
+    public static class Evaluator{
 
-    public delegate int Lookup(String v);
-    String[] substrings = Regex.Split(s, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+        public delegate int Lookup(String v);
+       
 
         
     
-
-      public static int Evaluate(String exp, Lookup variableEvaluator)
-      {
-        int x;
+/// <summary>
+/// A method that uses stacks to separate a string formula and perform operations on the values according to
+/// standard order of operations in arithmatic and return the value of the formula if the formula is valid.
+/// Throws an Argument Exception if the formula is invalid.
+/// </summary>
+/// <param name="exp"> A formula used to evaluate. </param>
+/// <param name="variableEvaluator"> A delegate that takes a string and returns the string value, used for variables in a formula. </param>
+/// <returns>Returns an int value of the solved formula. </returns>
+      public static int Evaluate(String exp, Lookup variableEvaluator){
+        Stack<int> numbers = new Stack<int>();
+        Stack<char> operators = new Stack<char>();
+        String[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+        int x = 0;
+        return x;
       }
 }
 
