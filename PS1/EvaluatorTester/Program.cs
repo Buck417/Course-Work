55b724b3417b501b6d86888b399e9933cee33813
Ryan Fletcher <buck417@gmail.com>
2015-09-17 23:58:30 -0600

    Final Submission

---
+++ PS1/EvaluatorTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FormulaEvaluator;
+
+
+namespace EvaluatorTester
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            String x = " (10-2)/4 - 1*500";
+             int answer =  Evaluator.Evaluate(x, s => 2);
+             int answer2 = Evaluator.Evaluate("2+3*5+(3+4*8)*5+2", s => 0);
+
+        }
+    }
+}
